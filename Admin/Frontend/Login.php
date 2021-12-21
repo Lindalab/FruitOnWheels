0089f02df5655824db8a7cb54be324ec6a560ff8
Linda Arthur <linda.arthur@ashesi.edu.gh>
2021-12-21 17:34:46 +0000

    Second commit,this include pictures

--- Admin/Frontend/Login.php
+++ Admin/Frontend/Login.php
@@ -4,14 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginstyle.css">
     <title>LOGIN</title>
 </head>
 <body>
     <header>
 
 
+      
 
+    </header>
 
-</header>
+    <div class="Form">
+          
+        <form method= "post" action="##">
+            <div class="Topic">
+                  <h2 id="Topic">Login</h2>
+                  <p> Fill to Login as an Admin</p>
+            </div>
+            <label for="email" style="margin-left:7%;" >Email </label><br>
+            <input type="email" placeholder="Email" name="email" required><br>
+
+            <label for="psw" style="margin-left:7%;" >Password</label><br>
+            <input type="password" placeholder="Email" name="psw" required><br><br>
+
+            <button  type="submit" id="Log"  name= "LoginSubmit" > Submit</button><br>
+        </form>
+    </div>
+
 </body>
 </html>
